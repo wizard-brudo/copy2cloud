73f4c0a6ba1ac482c192078e47b9398f370780ad
wizard-brudo <twentyoneandrey@gmail.com>
2022-01-14 11:58:27 +0200

    Подправлен install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 go build -o copy2cloud main.go
-=======
-go build -o copy2cloud main.go errors.go colors.go yadisk.go utils.go resources.go
->>>>>>> 6e6eb226087cd70441ee2922ecf10aec15135e77
 mv copy2cloud /usr/local/bin
